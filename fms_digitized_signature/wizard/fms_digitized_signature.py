19b799ddb381cae6418f1bdb5f3f69e918c28480
bilbonet.net <jesus@bilbonet.net>
2019-12-16 22:51:48 +0100

    [FIX] error when you save the expedition [ADD]Translation spanish

--- fms_digitized_signature/wizard/fms_digitized_signature.py
+++ fms_digitized_signature/wizard/fms_digitized_signature.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
-from base64 import b64decode
 
 
 class FmsDigitizedSignatureWizard(models.TransientModel):
