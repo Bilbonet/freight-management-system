521a7fd6a7c8152997a71c4b887b6859ab7bac3b
bilbonet <jesus@bilbonet.net>
2022-05-03 16:20:05 +0200

    [ADD] We add new field in employee form for deciding the type of calc of the commissions

--- fms/models/hr.py
+++ fms/models/hr.py
@@ -7,4 +7,17 @@ from odoo import fields, models
 class Employee(models.Model):
     _inherit = "hr.employee"
 
-    fms_commission = fields.Float(string='Percent of commission')
+    fms_type = fields.Selection(
+        string="FMS type of commision",
+        selection=[
+            ('percent', 'Percent'),
+            ('distribution', 'Distribution'),
+        ],
+        default='percent',
+        help=(
+            'How the commission amount is calculated:\n'
+            'Percent: Is a percent of the amount set in (Commission value) of the expedition.\n'
+            'Dsitribution: The amount set in (Commission value) is distributed between the commission lines.'
+        ),
+    )
+    fms_commission = fields.Float(string='Percent of commission', default=0.0)
