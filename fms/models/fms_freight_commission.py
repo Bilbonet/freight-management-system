b3ed86338df6ee6bba6af524b93dd9dda401e4e4
bilbonet <jesus@bilbonet.net>
2022-05-14 21:49:05 +0200

    [WIP] refactoring the calculatin of employee commissions

--- fms/models/fms_freight_commission.py
+++ fms/models/fms_freight_commission.py
@@ -93,28 +93,65 @@ class FmsFreightCommissionLine(models.Model):
             This calculation is only for employees with distribution commission type. 
             Commissions by percent type is calculated in 'onchange' method.
         """
-        emp = self.env['hr.employee'].browse(
+
+        """
+        Creating new distribution commission lines.
+        """
+        new_distri_lines = self.env['hr.employee'].browse(
                 [e['employee_id'] for e in vals]
             ).filtered(
                     lambda e: e.fms_type == 'distribution'
                 )
-        if emp:
-            freigth_obj = self.env['fms.freight'].browse(vals[0].get('freight_id'))
-            if freigth_obj.fr_commission != 0:
-                #TODO: Controlar si la expedicion ya tiene lineas con empleados a repartir comision.
-                #We look if there are previous commission lines
-                prev_lines = freigth_obj.commission_line_ids.filtered(
-                            lambda l: l.employee_id.fms_type == 'distribution'
+        """
+        We look if there are previous distribution commission lines.
+        """
+        freigth_obj = self.env['fms.freight'].browse(vals[0].get('freight_id'))
+        prev_distri_lines = freigth_obj.commission_line_ids.filtered(
+                    lambda l: l.employee_id.fms_type == 'distribution'
+                )
+
+        if new_distri_lines or prev_distri_lines:
+            amount = freigth_obj.fr_commission
+            if amount != 0:
+                """
+                We are creating employees with percent commission?
+                We need to discount the amount of commission before to distribute.
+                """
+                emp_percent = self.env['hr.employee'].browse(
+                        [e['employee_id'] for e in vals]
+                    ).filtered(
+                            lambda e: e.fms_type == 'percent'
+                        )              
+                if emp_percent:
+                    amount_percent = 0
+                    for ap in [l for l in vals if l['employee_id'] in emp_percent.ids]:
+                        amount_percent = amount_percent + ap.get('amount')
+
+                    if amount_percent != 0:
+                        amount = amount - amount_percent
+
+                """
+                Exists previous percent commissions?
+                We have to discount these too 
+                """
+                prev_percent_lines = freigth_obj.commission_line_ids.filtered(
+                            lambda l: l.employee_id.fms_type == 'percent'
                         )
-                distri_to = len(prev_lines) + len(emp)
-                amount = freigth_obj.fr_commission / distri_to
-                for line in [l for l in vals if l['employee_id'] in emp.ids]:
+                if prev_percent_lines:
+                    for ppl in prev_percent_lines:
+                        amount = amount - ppl.amount 
+
+                """
+                We distribute the amount between the new lines and the previous ones
+                """
+                distri_to = len(prev_distri_lines) + len(new_distri_lines)
+                amount = amount / distri_to
+                for line in [l for l in vals if l['employee_id'] in new_distri_lines.ids]:
                     line.update({'amount': amount})
                 
-                #Update previous lines
-                for line in prev_lines:
+                # Update previous lines
+                for line in prev_distri_lines:
                     line.write({
-                        'employee_id': line.employee_id.id,
                         'amount': amount
                     })
         
@@ -123,18 +160,70 @@ class FmsFreightCommissionLine(models.Model):
     def write(self, vals):
         """
         Commission calculation: 
-            This calculation is only for employees with distribution commission type. 
-            Commissions by percent type is calculated in 'onchange' method.
+            When we change any employee we have to recalculate distribution commissions
         """
-        if 'employee_id' in vals and not 'amount' in vals:
-            emp_obj = self.env['hr.employee'].browse(vals.get('employee_id'))
-            if  emp_obj.fms_type == 'distribution':
-                if self.freight_id.fr_commission != 0:
+        res = super(FmsFreightCommissionLine, self).write(vals)
+
+        if 'employee_id' in vals:
+            """
+            If there is any commission distribution lines, we have to distribute the commission
+            """
+            distri_lines = self.freight_id.commission_line_ids.filtered(
+                lambda l: l.employee_id.fms_type == 'distribution'
+            ) 
+
+            if distri_lines and self.freight_id.fr_commission != 0:
                     amount = self.freight_id.fr_commission
-                    distri_to = len(self.freight_id.commission_line_ids.filtered(
-                            lambda l: l.employee_id.fms_type == 'distribution'
-                        ))
-                    amount = amount / distri_to
-                    vals.update({'amount': amount})
-        
-        return super(FmsFreightCommissionLine, self).write(vals)
+                    """
+                    Exists lines with percent commissions?
+                    We have to discount these 
+                    """
+                    percent_lines = self.freight_id.commission_line_ids.filtered(
+                        lambda l: l.employee_id.fms_type == 'percent'
+                    )
+                    if percent_lines:
+                        for ppl in percent_lines:
+                            amount = amount - ppl.amount
+
+                    """
+                    We distribute the amount between the distribution commission lines
+                    """   
+                    amount = amount / len(distri_lines)
+                    distri_lines.update({
+                        'amount': amount
+                    })
+        return res
+
+    def unlink(self):
+        """
+        Commission calculation: 
+            When we delete any employee we have to recalculate distribution commissions.
+            If there is any commission distribution lines, we have to distribute the commission
+        """        
+        distri_lines = self.freight_id.commission_line_ids.filtered(
+            lambda l: l.employee_id.fms_type == 'distribution' and 
+                      l.employee_id != self.employee_id
+        )
+        if distri_lines and self.freight_id.fr_commission != 0:
+                amount = self.freight_id.fr_commission
+                """
+                Exists lines with percent commissions?
+                We have to discount these 
+                """
+                percent_lines = self.freight_id.commission_line_ids.filtered(
+                    lambda l: l.employee_id.fms_type == 'percent' and 
+                              l.employee_id != self.employee_id
+                )
+                if percent_lines:
+                    for ppl in percent_lines:
+                        amount = amount - ppl.amount
+
+                """
+                We distribute the amount between the rest of distribution commission lines
+                """   
+                amount = amount / len(distri_lines)
+                distri_lines.update({
+                    'amount': amount
+                })
+
+        return super(FmsFreightCommissionLine, self).unlink()
